12bc327416cfc458db29e3fa25b4391f6fe0834b
Mirko Cukich <mirkocukich@gmail.com>
2017-01-20 14:34:25 -0600

    winning positions multi-dim array added

--- app/src/main/java/com/digitalmirko/noughtsandcrosses/MainActivity.java
+++ app/src/main/java/com/digitalmirko/noughtsandcrosses/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
+    // All the winning positions
+    int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8}, // Horizontal Winning Positions
+                                {0,3,6} ,{1,4,7},{2,5,8}, // Vertical Winning Positions
+                                {0,4,8}, {2,4,6}};  // Angular Winning Positions
+
     public void dropIn(View view){
 
         // User taps on space, gets the image user tapped on
